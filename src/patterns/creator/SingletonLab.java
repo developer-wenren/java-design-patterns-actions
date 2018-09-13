6422b4c7d97e0222c80e83dd6cef66c27f56dc3c
wrcj12138aaa <wrcj12138@163.com>
2018-09-13 19:35:20 +0800

    Initial commit

---
+++ src/patterns/creator/SingletonLab.java
@@ -0,0 +1,13 @@
+package patterns.creator;
+
+/**
+ * 单例模式 Singleton
+ * @author One
+ * Coding By One ON 09/13
+ */
+public class SingletonLab {
+
+    public static void main(String[] args) {
+
+    }
+}
