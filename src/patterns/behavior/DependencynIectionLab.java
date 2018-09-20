9fceb2b4901ec10882e3e0e7f3e57950d29036c7
wrcj12138aaa <wrcj12138@163.com>
2018-09-20 23:01:56 +0800

    -依赖倒置原则

---
+++ src/patterns/behavior/DependencynIectionLab.java
@@ -0,0 +1,59 @@
+package patterns.behavior;
+
+/**
+ * @author One
+ * @description 依赖注入原则
+ * @date 2018/09/20
+ */
+public class DependencynIectionLab {
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        ICar car = new Benz();
+        driver.drive(car);
+        //Benz runing
+        car = new BMW();
+        driver.drive(car);
+        //BMW runing
+        car = new Audio();
+        driver.drive(car);
+        //Audio runing
+    }
+}
+
+/**
+ * 抽象接口-车
+ */
+interface ICar {
+    void run();
+}
+
+class Benz implements ICar {
+    @Override
+    public void run() {
+        System.out.println("Benz runing");
+    }
+}
+
+class Audio implements ICar {
+    @Override
+    public void run() {
+        System.out.println("Audio runing");
+    }
+}
+
+class BMW implements ICar {
+    @Override
+    public void run() {
+        System.out.println("BMW runing");
+    }
+}
+
+/**
+ * 功能方法依赖抽象,而不是实现类
+ */
+class Driver {
+    void drive(ICar car) {
+        car.run();
+    }
+}
